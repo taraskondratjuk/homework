8b118aab40de0f6b7c4a61969658397c359c25ce
TarasKondratiuk <taraskondratjuk@.gmail.com>
2020-11-03 14:08:00 +0200

    homework-7

---
+++ HTML/homework-7/task2/task2_1/myJS.js
@@ -0,0 +1,30 @@
+// --Завантажити з мережі будь-який шаблон сайту. Підключити до нього свій скріпт-файл. У файлі прописати наступні доступи та дії
+// - знайти всі елементі, які мають class
+// - знайти всі параграфи ,та змінити текст на hello oktenweb!
+// - знайти всі div та змінити ім колір на червоний
+
+
+// // - знайти всі елементі, які мають class
+//
+// let getAllElementsClass = document.querySelectorAll("*");
+//     console.log(getAllElementsClass);
+//
+//
+
+
+// - знайти всі параграфи ,та змінити текст на hello oktenweb!
+
+// let getAllParagraph = document.getElementsByTagName("p");
+// for (let i = 0; i < getAllParagraph.length; i++) {
+//     getAllParagraph[i].innerText = "hello oktenweb!";
+// }
+
+//
+//
+
+//  - знайти всі div та змінити ім колір на червоний
+
+// let getAllDiv = document.getElementsByTagName("div");
+// for (let i = 0; i < getAllDiv.length; i++) {
+//     getAllDiv[i].style.backgroundColor = "red"
+// }
